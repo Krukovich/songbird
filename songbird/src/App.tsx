c7125234583ce1d7167d7a58528046d853c0073f
Alexey <Shaht3r@gmail.com>
2020-07-29 23:11:09 +0300

    init: install create react app

---
+++ songbird/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App: React.FC = () => {
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-6">
+          </div>
+          <div className="col-12 col-lg-6">
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
